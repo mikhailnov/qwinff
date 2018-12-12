19b02801bd09986027e241e48366b3ca32072056
Mikhail Novosyolov <mikhailnov@dumalogiya.ru>
2018-12-12 10:45:05 +0300

    Initial build of deb package (for PPA)

---
+++ debian/build-ppa.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# I use this script to build and publish deb packages in ppa:mikhailnov/qwinff (https://launchpad.net/~mikhailnov/+archive/ubuntu/qwinff)
+# I publish it to allow other people to use it and make it possible to maintain a new PPA easily in case I stop doing it for some reason
+# I think, it can also be used for maintaining packages in mainline Debian (minor modifications required)
+
+pkg_name="qwinff"
+
+# this allows the script to be ran both from the root of the source tree and from ./debian directory
+dir_start="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ "$(basename "${dir_start}")" = 'debian' ]; then
+	cd ..
+fi
+
+debian/rules clean
+dir0="$(pwd)"
+old_header=$(head -1 ./debian/changelog)
+old_format_source=$(cat ./debian/source/format)
+
+for i in trusty xenial bionic cosmic disco
+do
+	old_version="$(cat ./debian/changelog | head -n 1 | awk -F "(" '{print $2}' | awk -F ")" '{print $1}')"
+	new_version="${old_version}~${i}1"
+	sed -i -re "s/${old_version}/${new_version}/g" ./debian/changelog
+	sed -i -re "1s/unstable/$i/" ./debian/changelog
+	#rm -fv ./debian/source/format
+	# -I to exclude .git; -d to allow building .changes file without build dependencies installed
+	dpkg-buildpackage -I -S -sa -d
+	sed  -i -re "1s/.*/${old_header}/" ./debian/changelog
+	#echo "$old_format_source" >./debian/source/format
+	cd ..
+	
+	# change PPA names to yours, you may leave only one PPA; I upload qwinff to 2 different PPAs at the same time
+	for ppa_name in ppa:mikhailnov/qwinff ppa:mikhailnov/utils
+	do
+		dput -f "$ppa_name" "$(/bin/ls -tr ${pkg_name}_*_source.changes | tail -n 1)"
+	done
+	
+	cd "$dir0"
+	sleep 1
+done
+
+debian/rules clean
+cd "$dir_start"
